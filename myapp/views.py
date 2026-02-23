e4b35b5e23b57b5db24ac28fdc93cef5f87334b1
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-02-24 00:16:35 +0530

    celery project

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    print('Home page')
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
+
